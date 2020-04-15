272980c5f9365bf77417ea14efd76823ce78aa11
hatsu38 <hajiwata0308@gmail.com>
2020-04-15 22:00:40 +0900

    Seedデータを作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Account.find_or_create_by(name: 'hatsu', code_name: 'hatsu38')
