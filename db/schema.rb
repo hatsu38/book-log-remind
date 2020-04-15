d0be09a9bcccf6d84a567a973b4c57286d0379c5
hatsu38 <hajiwata0308@gmail.com>
2020-04-15 17:13:09 +0900

    本テーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_15_080206) do
+ActiveRecord::Schema.define(version: 2020_04_15_080453) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,17 @@ ActiveRecord::Schema.define(version: 2020_04_15_080206) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "books", comment: "本", force: :cascade do |t|
+    t.string "title", null: false, comment: "タイトル"
+    t.integer "asin", comment: "ASINコード"
+    t.string "code", null: false, comment: "本コード(ASINがあるとは限らないため)"
+    t.string "image_url", comment: "画像URL"
+    t.bigint "author_id", null: false, comment: "著者ID"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["author_id"], name: "index_books_on_author_id"
+  end
+
   create_table "users", comment: "ユーザー", force: :cascade do |t|
     t.string "name", null: false, comment: "ユーザーの名前"
     t.string "code_name", null: false, comment: "ユーザーのコードネーム"
@@ -28,4 +39,5 @@ ActiveRecord::Schema.define(version: 2020_04_15_080206) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "books", "authors"
 end
