63ed0a9cf35a465367d9d556ec586259ceea38d2
hatsu38 <hajiwata0308@gmail.com>
2020-04-25 13:59:44 +0900

    レビューを保存

--- app/models/account.rb
+++ app/models/account.rb
@@ -3,4 +3,13 @@ class Account < ApplicationRecord
   has_many :books, through: :account_books
 
   validates :name, :code_name, presence: true
+
+  def registe_self_book(scrape_book, author)
+    books.find_or_create_by(
+      title: scrape_book['title'],
+      code: scrape_book['asin'],
+      image_url: scrape_book['image'],
+      author: author
+    )
+  end
 end
